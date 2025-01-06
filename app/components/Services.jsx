0821cc0b0e3846d9a37f9eb9663330481614d4e8
chhotugoud7 <chhotu957588@gmail.com>
2025-01-06 17:27:58 +0530

    services section is initilised or setup

---
+++ app/components/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const services = () => {
+  return (
+    <div>
+
+<h4 className='textce-center mb-2 text-lg font-Ovo'>Introduction</h4>
+<h2 className='text-center text-5xl font-Ovo'>About me</h2>
+    </div>
+  )
+}
+
+export default services     
