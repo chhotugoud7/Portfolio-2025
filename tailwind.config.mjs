ab3ef71a878377407d3dda76a115acec490c7dac
chhotugoud7 <chhotu957588@gmail.com>
2025-01-06 17:29:21 +0530

    three modified files are added like layout.js,page.js and tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,18 @@ export default {
       fontFamily: {
         Outfit: ["Outfit", "sans-serif"],
         Ovo: ["Ovo", "serif"],
+      },
+      boxShadow: {
+        'black' : '4px 4px 0 #000',
+        'white' : '4px 4px 0 #fff',
+        'green' : '4px 4px 0 #00FF00',
+        'green_circle' : '0 0 8px 4px #00FF00',
+        'blue_circle': '0 0 8px 4px #007BFF',
+        'purple_circle': '0 0 8px 4px #8A2BE2',
+        'yellow_circle': '0 0 8px 4px #FFD700',
+        'orange_circle': '0 0 8px 4px #FF5733',
+        'black_circle': '0 0 8px 4px #000000',
+         
       }
     },
   },
