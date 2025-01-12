9c69caddf732fc26cc4f22068b9096416b31d10c
chhotugoud7 <chhotu957588@gmail.com>
2025-01-12 10:42:36 +0530

    adding all pages to the page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import Services from "./components/Services";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
@@ -10,8 +11,8 @@ export default function Home() {
     <Navbar />
     <Header />
     <About />
-    <Services />
-    
+    <Skills />
+    <Projects/>
     </>
   );
 }
