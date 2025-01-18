1dc621c687915977184079597d2c1f4d0ba493bd
chhotugoud7 <chhotu957588@gmail.com>
2025-01-18 14:22:39 +0530

    other updates are done

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
@@ -13,6 +14,8 @@ export default function Home() {
     <About />
     <Skills />
     <Projects/>
+    <Contact/>
+    
     </>
   );
 }
