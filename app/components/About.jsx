f57f6f73b90ab589e232fcfc9627b6873a215650
chhotugoud7 <chhotu957588@gmail.com>
2025-01-26 01:03:41 +0530

    update dark mode in about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,7 @@ import { assets, infoList, toolsData } from '@/assets/assets'
 import Image from 'next/image'
 import React, { useState } from 'react'
 
-const About = () => {
+const About = ({isDarkMode}) => {
 
     const [showMore, setShowMore] = useState(false);
 
@@ -51,16 +51,16 @@ const About = () => {
 
                 <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                     {infoList.map(({icon, iconDark, title, description}, index) => (
-                        <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-green'
+                        <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-green dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'
                          key={index}>
-                            <Image src={icon} alt={title} className='w-7 mt-3'/>
-                            <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
-                            <p className='text-gray-600 text-sm'>{description}</p>
+                            <Image src={isDarkMode ? iconDark :icon} alt={title} className='w-7 mt-3'/>
+                            <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>{title}</h3>
+                            <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
                         </li>
                     ))}
                 </ul>
 
-                <h4>Tools I use</h4>
+                <h4 className='my-6 text-gr'>Tools I use</h4>
                 <ul className='flex items-center gap-3 sm:gap-5'>
                     {toolsData.map((tool, index)=>(
                         <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 hover:shadow-black ' 
